be223b8247478b5d328ef5c326d28df570d869e8
Krazoc <38321580+Krazoc@users.noreply.github.com>
2022-09-19 11:56:47 +0200

    Delete djangosandbox directory

--- djangosandbox/smallsite/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SmallsiteConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'smallsite'
--- djangosandbox/smallsite/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-app_name = 'smallsite'
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('object/', views.launch_object_search, name='objectresult'),
-]
